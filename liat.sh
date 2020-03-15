37f40b6c67ca57fa842d074fd6228304476c8a21
Codot-ID <61627130+Codot-ID@users.noreply.github.com>
2020-03-15 19:19:38 +0700

    Create liat.sh

---
+++ liat.sh
@@ -0,0 +1,6 @@
+toilet -f mono12 -F gay ngapain?
+toilet -f mono12 -F gay jangan
+toilet -f mono12 -F gay recode
+toilet -f mono12 -F gay bangke
+toilet -f mono12 -F gay susah
+toilet -f mono12 -F gay bikinya
